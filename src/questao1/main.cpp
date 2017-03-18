c88a35e29cbb6e10b7f2e9e683381c282de93aef
Bianca <bianca.rc@hotmail.com>
2017-03-18 16:02:59 -0300

    Consertando a precisão

--- src/questao1/main.cpp
+++ src/questao1/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <iomanip>
 #include "../include/questao1/rec.h"
 #include "../include/questao1/iterat.h"
 #include "../include/questao1/msg.h"
 
 using std::cout;
-using std::endl;
+using std::setprecision;
 
 int main (int argc, char* argv[]) {
 	argc = 4;
@@ -16,23 +17,23 @@ int main (int argc, char* argv[]) {
 	if (ab == 'A') {
 		if (ri == 'R') {
 			msgAB(ab, n);
-			cout << a_rec(n);
+			cout << setprecision(3) << a_rec(n);
 			msgRI(ri);
 		}
 		if (ri == 'I') {
 			msgAB(ab, n);
-			cout << a_iterat(n);
+			cout << setprecision(3) << a_iterat(n);
 			msgRI(ri);
 		}
 	} else if (ab == 'B') {
 		if (ri == 'R') {
 			msgAB(ab, n);
-			cout << b_rec(n);
+			cout << setprecision(3) << b_rec(n);
 			msgRI(ri);
 		}
 		if (ri == 'I') {
 			msgAB(ab, n);
-			cout << b_iterat(n);
+			cout << setprecision(3) << b_iterat(n);
 			msgRI(ri);
 		}
 	}
